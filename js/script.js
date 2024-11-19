75cc71488f83d6b276cd3031508b69b05b8a9ed9
mahoudcha <37213912+mahoudcha@users.noreply.github.com>
2024-11-18 19:46:04 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Mahoudcha-Trade site is ready!');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Site Mahoudcha-Trade chargé !');
+    
+    // Ajoutez ici vos fonctionnalités JavaScript
+});
